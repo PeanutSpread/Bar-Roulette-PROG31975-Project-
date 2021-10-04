85fd421501e30f8e8c4555ea5e68359f03bfa960
Luke Van Rooyen <lukevanrooyen@hotmail.com>
2021-10-03 22:48:39 -0400

    Created blank project

---
+++ Bar Roulette/Bar Roulette/Bar_RouletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bar_RouletteApp.swift
+//  Bar Roulette
+//
+//  Created by Luke Van Rooyen on 2021-10-03.
+//
+
+import SwiftUI
+
+@main
+struct Bar_RouletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bar Roulette/Bar Roulette/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bar Roulette
+//
+//  Created by Luke Van Rooyen on 2021-10-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
