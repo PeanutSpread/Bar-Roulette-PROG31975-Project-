97b0746e144cff49f816d1e1fd1e5247a525a996
Luke Van Rooyen <lukevanrooyen@hotmail.com>
2021-11-08 13:19:02 -0500

    added a simple bar class

--- Bar Roulette/Models/Bar.swift
+++ Bar Roulette/Models/Bar.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+class Bar {
+    private var name : String
+    private var type : String
+    private var rating : Double
+    private var latitude : Double
+    private var longitude : Double
+    private var address : String
+    private var phone : String
+    private var website : String
+    
+    init (Name name : String, Type type : String, Rating rating : Double, Latitude latitude : Double, Longitude longitude : Double, Address address : String, Phone phone : String, Website website : String) {
+        
+        self.name = name
+        self.type = type
+        self.rating = rating
+        self.latitude = latitude
+        self.longitude = longitude
+        self.address = address
+        self.phone = phone
+        self.website = website
+        
+    }
+    
+    
+}
