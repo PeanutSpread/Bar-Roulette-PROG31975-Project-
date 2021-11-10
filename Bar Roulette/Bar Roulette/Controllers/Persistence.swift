784acfbef1b14e1258e45b88494fa2da53182d6e
Akochhar29 <aks.kochhar@gmail.com>
2021-11-10 15:57:22 -0500

    Created Persistence Controller with database name as BarDB

---
+++ Bar Roulette/Bar Roulette/Controllers/Persistence.swift
@@ -0,0 +1,37 @@
+//
+//  Persistence.swift
+//  Bar Roulette
+//
+//  Created by Akshay Kochhar on 2021-11-10.
+//
+
+import Foundation
+import CoreData
+
+struct PersistenceController {
+    //singleton instance
+    static let shared = PersistenceController()
+    
+    static var preview: PersistenceController = {
+        let result = PersistenceController(inMemory: true)
+        let viewContext = result.container.viewContext
+        return result
+    }()
+    
+    let container : NSPersistentContainer
+    
+    init(inMemory: Bool = false){
+        container = NSPersistentContainer(name: "BarDB")
+        
+        if inMemory{
+            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        }
+        
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError?{
+                print("Unable to access BarDB. \(error)")
+            }
+        })
+    }
+    
+}
