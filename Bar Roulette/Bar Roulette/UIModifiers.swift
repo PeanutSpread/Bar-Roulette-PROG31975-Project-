48f149fbacc40de8ad7d46fc0eaadea5eaded866
Luke Van Rooyen <lukevanrooyen@hotmail.com>
2021-11-10 20:02:24 -0500

    The unfudgening

--- Bar Roulette/Bar Roulette/UIModifiers.swift
+++ Bar Roulette/Bar Roulette/UIModifiers.swift
@@ -4,28 +4,65 @@
 //
 //  Created by Luke Van Rooyen on 2021-11-08.
 //
-
 import SwiftUI
-
+extension Color {
+    static let orange_yellow = Color(red: 243 / 255, green: 183 / 255, blue: 0 / 255) // F3B700
+    static let honeydew = Color(red: 208 / 255, green: 225 / 255, blue: 212 / 255) // D0E1D4
+    static let eerie_black = Color(red: 38 / 255, green: 38 / 255, blue: 38 / 255) // 262626
+}
+extension UIColor {
+    static let orange_yellow = UIColor(red: 243 / 255, green: 183 / 255, blue: 0 / 255, alpha: 1) // F3B700
+    static let honeydew = UIColor(red: 208 / 255, green: 225 / 255, blue: 212 / 255, alpha: 1) // D0E1D4
+    static let eerie_black = UIColor(red: 38 / 255, green: 38 / 255, blue: 38 / 255, alpha: 1) // 262626
+}
 public var screenWidth: CGFloat { return UIScreen.main.bounds.width } // Screen width.
 public var screenHeight: CGFloat { return UIScreen.main.bounds.height } // Screen height.
-
 struct RouletteButtonModifier: ViewModifier{
     func body(content: Content) -> some View {
         return content
-            .frame(width: screenWidth * (2/3), height: screenHeight * (1/6))
-            .background(Color.red)
-            .cornerRadius(20)
+            .font(.title)
             .padding(12)
+            .background(Color.orange_yellow)
+            .cornerRadius(20)
+            .overlay(
+                RoundedRectangle(cornerRadius: 20)
+                    .stroke(Color.honeydew, lineWidth: 5)
+            )
     }
 }
-
 struct RouletteButtonTextModifier: ViewModifier{
     func body(content: Content) -> some View {
         return content
-            .foregroundColor(Color.black)
+            .foregroundColor(Color.eerie_black)
             .font(.largeTitle)
             .padding(.horizontal, 12)
             .padding(.vertical, 10)
+    }
+}
+struct RouletteFavouritesModifier: ViewModifier{
+    func body(content: Content) -> some View {
+        return content
+            .foregroundColor(Color.honeydew)
+            .font(.system(size: screenWidth/10))
+            .overlay(
+                Image(systemName: "star.fill")
+                    .font(.system(size: screenWidth/15))
+                    .foregroundColor(Color.orange_yellow)
+            )
+    }
+}
+struct RouletteNavigationModifier: ViewModifier{
+    func body(content: Content) -> some View {
+        return content
+            .accentColor(Color.eerie_black)
+            .background(Color.eerie_black)
+    }
+}
+
+struct RouletteBackgroundModifier: ViewModifier{
+    func body(content: Content) -> some View {
+        return content
+            .background(Color.eerie_black)
+            .accentColor(Color.eerie_black)
     }
 }
--- Bar Roulette/Bar Roulette/Views/ContentView.swift
+++ Bar Roulette/Bar Roulette/Views/ContentView.swift
@@ -4,29 +4,36 @@
 //
 //  Created by Luke Van Rooyen on 2021-10-03.
 //
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showBar = false
+
+    init(){
+        UINavigationBar.appearance().backgroundColor = .eerie_black
+    }
+
     var body: some View {
         NavigationView {
             VStack {
                 if(showBar) {
                     //BarView()
-                    
+
                 } else {
                     Button(action: {showBar = true}){Text("Bar Me").modifier(RouletteButtonTextModifier())}
                     .modifier(RouletteButtonModifier())
                 }
-            
-            FavouritesView().tabItem{
-                Image(systemName: "star.fill")
-                Text("Favourites")
-            }.padding(10)
+
+            }
+            .modifier(RouletteBackgroundModifier())
+            .toolbar{
+                Button(action: {})
+                {Image(systemName: "star.fill").modifier(RouletteFavouritesModifier())}
+            }
         }
     }
 }
-
+  
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
