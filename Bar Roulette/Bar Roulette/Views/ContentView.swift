b6dfd8ed9ef7bf32aee4b813abbe8070cdcc28b5
Luke Van Rooyen <lukevanrooyen@hotmail.com>
2021-12-12 07:55:11 -0500

    RE-added landmark stuff to content view which went missing for some reason

--- Bar Roulette/Bar Roulette/Views/ContentView.swift
+++ Bar Roulette/Bar Roulette/Views/ContentView.swift
@@ -5,14 +5,44 @@
 //  Created by Luke Van Rooyen on 2021-10-03.
 //
 import SwiftUI
+import MapKit
+
+let SEARCH = "bar"
 
 struct ContentView: View {
-    @EnvironmentObject var coreDBH : CoreDBHelper
+    @EnvironmentObject var coreDBH: CoreDBHelper
+    @EnvironmentObject var locationHelper: LocationHelper
+    @State private var landmarks: [LandMark] = [LandMark]()
+    @State private var bars: [Bar] = [Bar]()
     @State private var showBar = false
     @State private var selection: Int? = 0
 
     init(){
         UINavigationBar.appearance().backgroundColor = .eerie_black
+    }
+    
+    func getNearbyBars() {
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = SEARCH
+        
+        let search = MKLocalSearch(request: request)
+        search.start { (response, error) in
+            if let response = response {
+                
+                let mapItems = response.mapItems
+                self.landmarks = mapItems.map {
+                    LandMark(placemark: $0.placemark)
+                }
+            }
+        }
+        
+        landmarks.forEach { pub in
+            //TODO: get more info on the location (i.e. rating)
+        }
+    }
+    
+    func shuffleAndDeal() {
+        //TODO: randomize the bar list
     }
 
     var body: some View {
